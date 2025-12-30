945f3ac8b96cfe14c6bcf83efc428d4745bcdb57
shubham-hundalekar <shubhamdh32@gmail.com>
2025-12-30 14:59:48 +0530

    exploring dynamic routing

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 
 //Response Handler
-app.use("/test",(req, res)=>{
-    res.send("Hello, DevTinder!");
+app.get("/user",(req, res)=>{
+    console.log(req.query.userId)
+    console.log(req.query.name)
+    res.send({FirstNaame: "Shubham", LastName: "Hundalekar"});
 })
-
-app.use("/home",(req, res)=>{
-    res.send("Welcome to the Home Page!");
+app.get("/user/:userId/:password/:name",(req, res)=>{
+    console.log(req.params)
+    res.send("data received through params");    
+})
+app.post("/user",(req, res)=>{
+    res.send("Data Saved Successfully in Database");
 });
 
 app.listen(3000, () => {
