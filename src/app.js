a56621420f444b486df293ac8ac39fe4d382eb1f
shubham-hundalekar <shubhamdh32@gmail.com>
2026-01-02 18:01:32 +0530

    Updating the data using PATCH API

--- src/app.js
+++ src/app.js
@@ -51,6 +51,19 @@ app.delete("/user", async (req,  res)=>{
     }
 })
 
+app.patch("/user",async (req, res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+
+    try{
+        await User.findByIdAndUpdate({_id:userId}, data);
+        res.send("Data updated successfully");
+    }catch(err){
+        res.status(404).send("Something went wrong");
+    }
+
+})
+
 connectDB().then(() => {
     console.log("Connection established successfully");
         app.listen(3000, () => {
