8c16129b39b52cf07cec3a0d11c744872a7cd674
shubham-hundalekar <shubhamdh32@gmail.com>
2026-01-02 10:37:29 +0530

    Created a User instance and added in User collection using post API in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
+const User = require("./models/user");
+
+app.post("/signup", async (req,  res)=>{
+    const userObj = {
+        firstName: "Shubham",
+        lastName: "Hundalekar",
+        emailId:"shubhamdh32@gmail.com",
+        password: "shubham@123"
+
+    }
+    const user = new User(userObj);
+    await user.save();
+    res.send("User added successfully");
+})
+
 
 
 connectDB().then(() => {
