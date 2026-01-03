43ea76d6534f269eaeaef16cbcf9809263eb375f
shubham-hundalekar <shubhamdh32@gmail.com>
2026-01-03 16:52:08 +0530

    Samll mistake in patch API refer previous commit for the mistake and present for correction

--- src/app.js
+++ src/app.js
@@ -51,22 +51,20 @@ app.delete("/user", async (req,  res)=>{
     }
 })
 
-app.patch("/user",async (req, res)=>{
-    const userId = req.body.userId;
+app.patch("/user/:userId",async (req, res)=>{
+    const userId = req.params?.userId;
     const data = req.body;
     try{
         const ALLOWED_UPDATES = [
-            "userId",
             "age",
             "skills",
             "about",
             "gender"
         ]
 
-        isUpdateAllowed = Object.keys(data).every((k)=>{
+        isUpdateAllowed = Object.keys(data).every((k)=>(
             ALLOWED_UPDATES.includes(k)
-        })
-
+        ))
         if(!isUpdateAllowed){
             throw new Error("Update not allowed");
         }
