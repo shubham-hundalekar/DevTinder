d1341fae60daece1981b389b1ae1c05b995dff30
shubham <shubhamhundalekar62668@gmail.com>
2026-09-15 22:40:40 +0530

    /user/connections (displays loggedIn user connections)

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const userRouter = express.Router();
 const userAuth = require("../middleware/userAuth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_DATA = "firstName lastName age gender photourl"
+
 //get all the pending connection request
 userRouter.get("/user/requests/received", userAuth, async (req, res)=>{
     try{
@@ -10,7 +12,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res)=>{
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status:"interested"
-        }).populate("fromUserId", ["firstName", "lastName"]);
+        }).populate("fromUserId", USER_SAFE_DATA);
         
         res.json({
             message:"Data fetched sucessfully",
@@ -20,6 +22,41 @@ userRouter.get("/user/requests/received", userAuth, async (req, res)=>{
 
     }catch(error){
         res.status(400).send("Error: "+error.message);
+    }
+})
+
+
+userRouter.get("/user/connections", userAuth,  async (req,res)=>{
+    try{
+        const loggedInUser = req.user;
+        
+        const connectionRequest = await ConnectionRequest.find({
+            $or:[{
+                    toUserId : loggedInUser._id,
+                    status: "accepted"
+                },
+                {
+                    fromUserId : loggedInUser._id,
+                    status : "accepted"
+                },
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA);
+
+        const data = connectionRequest.map((row)=>{
+            if(loggedInUser._id.toString() === row.fromUserId._id.toString())
+                return row.toUserId;
+            return row.fromUserId;
+        })
+
+
+        res.json({
+            data
+        })
+
+
+    }catch(error){  
+        res.status(400).send({message: error.message});
     }
 })
 
